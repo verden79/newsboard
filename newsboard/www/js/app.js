7573a9870e05fb0ad98d2103ae38dfe90cbc78ab
3nGercog <sereqa0813@rambler.ru>
2016-06-30 21:56:12 +0300

    my change

--- newsboard/www/js/app.js
+++ newsboard/www/js/app.js
@@ -7,10 +7,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    //if(window.cordova && window.cordova.plugins.Keyboard) {
       //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       //cordova.plugins.Keyboard.disableScroll(true);
-    }
+    //}
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
